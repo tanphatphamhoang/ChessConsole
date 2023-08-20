61c2c8451be70cc7d5cc97f46becd98f700ed79f
phuongdoan13 <mphuongdoant@gmail.com>
2023-08-20 20:45:36 +1000

    Update: Update test

--- Parser/Parser.test.ts
+++ Parser/Parser.test.ts
@@ -1,26 +1,18 @@
 import Parser from './Parser';
 
 describe('Parser checkIfInputIsStandardized', () => {
-  test('should pass standardized move for pawn', () => {
+  test('should pass standardized moves', () => {
     const parser = Parser.getInstance();
-    expect(parser.checkIfInputIsStandardized("a1")).toBe(true);
-  });
+    expect(parser.checkIfInputIsStandardized("a1a2")).toBe(true);
+    expect(parser.checkIfInputIsStandardized("b2c3")).toBe(true);
+    expect(parser.checkIfInputIsStandardized("a1h8")).toBe(true);
 
-  test('should pass standardized moves for KQBNR', () => {
-    const parser = Parser.getInstance();
-    expect(parser.checkIfInputIsStandardized("Kbb1")).toBe(true);
-    expect(parser.checkIfInputIsStandardized("Qbb1")).toBe(true);
-    expect(parser.checkIfInputIsStandardized("Bbb1")).toBe(true);
-    expect(parser.checkIfInputIsStandardized("Nbb1")).toBe(true);
-    expect(parser.checkIfInputIsStandardized("Rbb1")).toBe(true);
   });
 
   test('should not pass non-standardized moves', () => {
     const parser = Parser.getInstance();
     expect(parser.checkIfInputIsStandardized("")).toBe(false);
-    expect(parser.checkIfInputIsStandardized("a")).toBe(false);
-    expect(parser.checkIfInputIsStandardized("ab1")).toBe(false);
-    expect(parser.checkIfInputIsStandardized("Kb1")).toBe(false);
-    expect(parser.checkIfInputIsStandardized("kbb1")).toBe(false);
+    expect(parser.checkIfInputIsStandardized("a1aa")).toBe(false);
+    expect(parser.checkIfInputIsStandardized("a1a1")).toBe(false);
   });
 });
