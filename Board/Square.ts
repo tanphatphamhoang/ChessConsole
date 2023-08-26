fc134c13a83ef117322e0f1641e5c5d11c2bea45
phuongdoan13 <mphuongdoant@gmail.com>
2023-08-26 22:09:27 +1000

    Add: class

---
+++ Board/Square.ts
@@ -0,0 +1,28 @@
+class Square{
+  position: Position;
+  piece: Piece | null = null;
+
+  public constructor(position: Position, piece: Piece | null = null){
+    this.position = position;
+    this.piece = piece;
+  }
+
+  public isEmpty(): boolean{
+    return this.piece == null;
+  }
+
+  public getPosition(): Position{
+    return this.position;
+  }
+
+  public getPiece(): Piece | null{
+    return this.piece;
+  }
+
+  public setPiece(piece: Piece): void{
+    this.piece = piece;
+  }
+
+  // tbd: toString();
+}
+
