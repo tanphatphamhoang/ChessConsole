6ba65dde9f426332b12e080bc03c8d44a50aabaa
tanphatphamhoang <tanphatphamhoang@gmail.com>
2023-09-02 12:48:16 +0700

    Add: class Piece

--- Piece/Piece.ts
+++ Piece/Piece.ts
@@ -1,3 +1,25 @@
-interface Piece {
-  
+interface Piece{
+    PieceType: PieceType; //string
+    hasMoved: boolean;
+    color:Color //string
+    increaments: array<array<number>>;
+    abtract private get_threatened_positions(piecetype: PieceType, position: number[]): array<array<number>{return [[]]};
+    abtract private get_movable_positions(piecetype: PieceType, position: number[]): array<Array<number>>{return [[]]}
 }
+
+enum PieceType {
+    Rook="rook",
+    Knight = "knight",
+    Bishop = "bishop",
+    Queen = "queen",
+    King = "king",
+    Pawn = "pawn"
+}
+
+let hasMoved: boolean;
+
+enum Color{
+    White="white",
+    Black="Black"
+}
+
