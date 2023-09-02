9043679ab31f32176422805206646deead7ef1fd
tanphatphamhoang <tanphatphamhoang@gmail.com>
2023-09-02 13:27:15 +0700

    Add: interface Piece

--- Piece/Piece.ts
+++ Piece/Piece.ts
@@ -1,25 +1,8 @@
-interface Piece{
-    PieceType: PieceType; //string
-    hasMoved: boolean;
-    color:Color //string
-    increaments: array<array<number>>;
-    abtract private get_threatened_positions(piecetype: PieceType, position: number[]): array<array<number>{return [[]]};
-    abtract private get_movable_positions(piecetype: PieceType, position: number[]): array<Array<number>>{return [[]]}
+interface Piece {
+    PieceType: PieceType;
+    hasMoved: Boolean;
+    Color: Color;
+    increaments: Array<Array<Number>>;
+    abtract private getThreatenedPositions(piecetype: PieceType, position: Number[]): Array<Array<Number>>{return [[]]};
+    abtract private getMovablePositions(piecetype: PieceType, position: Number[]): Array<Array<Number>>{return [[]]}
 }
-
-enum PieceType {
-    Rook="rook",
-    Knight = "knight",
-    Bishop = "bishop",
-    Queen = "queen",
-    King = "king",
-    Pawn = "pawn"
-}
-
-let hasMoved: boolean;
-
-enum Color{
-    White="white",
-    Black="Black"
-}
-
---
+++ Piece/PieceType.ts
@@ -0,0 +1,13 @@
+enum PieceType {
+    Rook= "rook",
+    Knight = "knight",
+    Bishop = "bishop",
+    Queen = "queen",
+    King = "king",
+    Pawn = "pawn"
+}
+
+enum Color {
+    White= "white",
+    Black= "Black"
+}
