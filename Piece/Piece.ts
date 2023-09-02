bdde69aa0bc2262ea94949f4c74c56f905bc0ecc
phuongdoan13 <mphuongdoant@gmail.com>
2023-09-02 17:27:50 +1000

    Update: comment out unused fields of interface Piece

--- Piece/Piece.ts
+++ Piece/Piece.ts
@@ -1,6 +1,13 @@
 import Color from "./Color";
+// import PieceType from "./PieceType";
+// import Position from "../Board/Position";
 
 export default interface Piece {
-  color: Color;
-  getColor(): Color;
+	Color: Color;
+	getColor(): Color;
+	// hasMoved: Boolean;
+	// PieceType: PieceType;
+	// increaments: Array<Array<Number>>;
+	// getThreatenedPositions(piecetype: PieceType, position: Position): Array<Position>;
+	// getMovablePositions(piecetype: PieceType, position: Position): Array<Position>;
 }
