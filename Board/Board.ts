eea876bf19475831c5435725c077d5b376e857af
phuongdoan13 <mphuongdoant@gmail.com>
2023-09-02 15:22:10 +1000

    Fix: use let instead of var in for

--- Board/Board.ts
+++ Board/Board.ts
@@ -1,16 +1,16 @@
-class Board{
+class Board {
     board: Array<Array<Square>>;
     readonly N = 8;
 
-    constructor(){
+    constructor() {
       this.board = this.initializeBoard();
     }
 
-    private initializeBoard(): Array<Array<Square>>{
+    private initializeBoard(): Array<Array<Square>> {
       var board!: Array<Array<Square>>;
-      for(var r = 0; r < this.N; r++){
+      for(let r = 0; r < this.N; r++){
         board.push(new Array<Square>());
-        for(var c = 0; c < this.N; c++){
+        for(let c = 0; c < this.N; c++){
           board[r].push(new Square(new Position(r, c)));
         }
       }
