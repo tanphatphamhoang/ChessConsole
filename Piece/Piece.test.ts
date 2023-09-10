d73a1c338e32319c95701433c4622b8844afb981
tanphatphamhoang <tanphatphamhoang@gmail.com>
2023-09-10 14:15:16 +0700

    Update: update format and eliminate some tests

--- Piece/Piece.test.ts
+++ Piece/Piece.test.ts
@@ -1,4 +1,3 @@
-import Piece from "./Piece"
 import DummyPiece from "./DummyPiece";
 import PieceType from "./PieceType";
 import Color from "./Color";
@@ -7,55 +6,34 @@ import Position from "../Board/Position";
 import Square from "../Board/Square";
 
 describe("Piece", () => {
-    let pieceRookWhite = new DummyPiece(PieceType.ROOK,Color.WHITE,[[]]);
-    let pieceKnightWhite = new DummyPiece(PieceType.KNIGHT,Color.WHITE,[[]]);
-    let pieceBishopWhite = new DummyPiece(PieceType.BISHOP,Color.WHITE,[[]]);
-    let pieceQueenWhite = new DummyPiece(PieceType.QUEEN,Color.WHITE,[[]]);
-    let pieceKingWhite = new DummyPiece(PieceType.KING,Color.WHITE,[[]]);
-    let piecePawnWhite = new DummyPiece(PieceType.PAWN,Color.WHITE,[[]]);
+  let pieceRookWhite = new DummyPiece(PieceType.ROOK, Color.WHITE, [[]]);
 
-    it("should return the correct piece type", () => {
-        expect(pieceRookWhite.getPieceType).toBe("rook");
-        expect(pieceKnightWhite.getPieceType).toBe("knight");
-        expect(pieceBishopWhite.getPieceType).toBe("bishop");
-        expect(pieceQueenWhite.getPieceType).toBe("queen");
-        expect(pieceKingWhite.getPieceType).toBe("king");
-        expect(piecePawnWhite.getPieceType).toBe("pawn");
-    })
+  it("should return the correct piece type", () => {
+    expect(pieceRookWhite.getPieceType).toBe("rook");
+  });
 
-    it("should return the correct intial hasMoved status", () => {
-        expect(pieceRookWhite.getHasMoved).toBe(false);
-        expect(pieceKnightWhite.getHasMoved).toBe(false);
-        expect(pieceBishopWhite.getHasMoved).toBe(false);
-        expect(pieceQueenWhite.getHasMoved).toBe(false);
-        expect(pieceKingWhite.getHasMoved).toBe(false);
-        expect(piecePawnWhite.getHasMoved).toBe(false);
-    })
+  it("should return the correct intial hasMoved status", () => {
+    expect(pieceRookWhite.getHasMoved).toBe(false);
+  });
 
-    it("should return the correct hasMoved status after the first move", () => {
-        let board = new Board();
-        let from = new Square(new Position(0, 0), pieceRookWhite);
-        let to = new Square(new Position(1, 1));
-        board.board[0][0] = from;
-        board.board[1][1] = to;
+  it("should return the correct hasMoved status after the first move", () => {
+    let board = new Board();
+    let from = new Square(new Position(0, 0), pieceRookWhite);
+    let to = new Square(new Position(1, 1));
+    board.board[0][0] = from;
+    board.board[1][1] = to;
+    board.executeMove(from, to);
 
-        board.executeMove(from,to);
+    expect(pieceRookWhite.getHasMoved).toBe(true);
+  });
 
-        expect(pieceRookWhite.getHasMoved).toBe(true);
-    })
+  it("should return the correct color", () => {
+    expect(pieceRookWhite.getColor).toBe("white");
+  });
 
-    it("should return the correct color", () => {
-        expect(pieceRookWhite.getColor).toBe("white");
-        expect(pieceKnightWhite.getColor).toBe("white");
-        expect(pieceBishopWhite.getColor).toBe("white");
-        expect(pieceQueenWhite.getColor).toBe("white");
-        expect(pieceKingWhite.getColor).toBe("white");
-        expect(piecePawnWhite.getColor).toBe("white");
-    })
-
-    it("should return the correct increments set", () => {
-        expect(JSON.stringify(pieceRookWhite.getIncrements)).toBe(JSON.stringify([[]]));
-    })
-    
-}
-)
+  it("should return the correct increments set", () => {
+    expect(JSON.stringify(pieceRookWhite.getIncrements)).toBe(
+      JSON.stringify([[]])
+    );
+  });
+});
--- Piece/Piece.ts
+++ Piece/Piece.ts
@@ -1,43 +1,42 @@
 import PieceType from "./PieceType";
 import Color from "./Color";
-import Position from "../Board/Position";
 
 export default abstract class Piece {
-        private pieceType: PieceType;
-        private hasMoved: boolean;
-        private color: Color;
-        private increments: Array<Array<Number>>;
-    // abstract public getThreatenedPositions(piecetype: PieceType, position: Position): Array<Position>;
-    // abstract public getMovablePositions(piecetype: PieceType, position: Position): Array<Position>;
+  private pieceType: PieceType;
+  private hasMoved: boolean;
+  private color: Color;
+  private increments: Array<Array<Number>>;
+  // abstract public getThreatenedPositions(piecetype: PieceType, position: Position): Array<Position>;
+	// abstract public getMovablePositions(piecetype: PieceType, position: Position): Array<Position>;
     
-    constructor(
-        pieceType: PieceType,
-        color: Color,
-        increments: Array<Array<Number>>){
-            this.pieceType = pieceType;
-            this.hasMoved = false;
-            this.color = color;
-            this.increments = increments
-    }
+  constructor(
+    pieceType: PieceType,
+    color: Color,
+    increments: Array<Array<Number>>){
+      this.pieceType = pieceType;
+      this.hasMoved = false;
+      this.color = color;
+      this.increments = increments
+  }
 
-    get getPieceType(): PieceType {
-        return this.pieceType;
-    }
+  get getPieceType(): PieceType {
+    return this.pieceType;
+  }
 
-    get getHasMoved(): Boolean {
-        return this.hasMoved;
-    }
+	get getHasMoved(): Boolean {
+    return this.hasMoved;
+  }
 
-    get getColor(): Color {
-        return this.color;
-    }
+  get getColor(): Color {
+    return this.color;
+  }
 
-    get getIncrements(): Array<Array<Number>> {
-        return this.increments;
-    }
+  get getIncrements(): Array<Array<Number>> {
+    return this.increments;
+  }
 
-    setHasMoved() {
-        this.hasMoved = true;
-    }
+  setHasMoved() {
+    this.hasMoved = true;
+  }
 
 }
