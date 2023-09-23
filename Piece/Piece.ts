7166ce3d55ab9507c0088930fd6b372d4849efcf
tanphatphamhoang <tanphatphamhoang@gmail.com>
2023-09-23 19:53:18 +0700

    Update: Elimate PieceType and Increments in class Piece and relevant code.

--- Piece/Piece.ts
+++ Piece/Piece.ts
@@ -1,26 +1,15 @@
-import PieceType from "./PieceType";
 import Color from "./Color";
 import Position from "../Board/Position";
 
 export default abstract class Piece {
-  private _pieceType: PieceType;
   private _hasMoved: boolean = false;
   private _color: Color;
-  private _increments: Array<Array<number>>;
   public abstract getThreatenedPositions(position: Position, increments: Array<Array<number>>, color: Color): Array<Position> ;
 	public abstract getMovablePositions(position: Position, increments: Array<Array<number>>, color: Color): Array<Position> ;
     
   constructor(
-    _pieceType: PieceType,
-    _color: Color,
-    _increments: Array<Array<number>>){
-      this._pieceType = _pieceType;
+    _color: Color,){
       this._color = _color;
-      this._increments = _increments
-  }
-
-  get pieceType(): PieceType {
-    return this._pieceType;
   }
 
 	get hasMoved(): Boolean {
@@ -29,10 +18,6 @@ export default abstract class Piece {
 
   get color(): Color {
     return this._color;
-  }
-
-  get increments(): Array<Array<number>> {
-    return this._increments;
   }
 
   public set hasMoved(hasMove : boolean) {
